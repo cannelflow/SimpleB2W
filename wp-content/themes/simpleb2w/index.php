0c3e8adc8accb15a00eb7b1ce3cd5774501fd3fc
cannelflow <mightyrocket@hotmail.com>
2017-01-13 19:23:16 +0000

    Lecture 6

--- wp-content/themes/simpleb2w/index.php
+++ wp-content/themes/simpleb2w/index.php
@@ -101,24 +101,27 @@
     <!-- About Section -->
     <section class="success" id="about">
         <div class="container">
+            
+            <?php
+              $args = array(
+                     'page_id' => '24',
+                  );
+              $query = new WP_Query($args);      
+            ?>
+            
+            <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+            <?php endwhile; else : ?>
+	        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+            
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2>About</h2>
+                    <h2><?php the_title() ; ?></h2>
                     <hr class="star-light">
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 col-lg-offset-2">
-                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
-                </div>
-                <div class="col-lg-4">
-                    <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
-                </div>
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <a href="#" class="btn btn-lg btn-outline">
-                        <i class="fa fa-download"></i> Download Theme
-                    </a>
-                </div>
+                <?php the_content() ?>
             </div>
         </div>
     </section>
